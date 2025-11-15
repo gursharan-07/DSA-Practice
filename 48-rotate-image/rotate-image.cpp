968a54c10b4a92821081b6ba2ea01ea02fb0c978
gursharan-07 <muschab45@gmail.com>
2025-11-15 12:39:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (34.89%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<int>> newarr(n,vector<int>(n));
         for(int r=0;r<n;r++){
-            for(int c=0;c<n;c++){
-                newarr[c][n-r-1]=matrix[r][c];
+            for(int c=r;c<n;c++){
+                swap(matrix[r][c],matrix[c][r]);
             }
         }
         for(int r=0;r<n;r++){
-            for(int c=0;c<n;c++){
-                cout<<newarr[r][c]<<" ";
-            }
-            cout<<endl;
+            reverse(matrix[r].begin(),matrix[r].end());
         }
-        matrix=newarr;
     }
 };
