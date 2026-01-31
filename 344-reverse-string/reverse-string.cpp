f70edada6f3a329f7c6357724864c5406cbb67f5
gursharan-07 <muschab45@gmail.com>
2026-01-31 13:57:43 +0530

    Time: 1 ms (10.06%) | Memory: 27.1 MB (97.54%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -5,9 +5,7 @@ public:
         int i=0;
         int j=n-1;
         while(i<j){
-            int temp=s[i];
-            s[i]=s[j];
-            s[j]=temp;
+            swap(s[i],s[j]);
             i++;
             j--;
         }
